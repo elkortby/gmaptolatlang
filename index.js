f36dfb889a608b5c66db440decf7979d383b595b
elkortby <kortbyoussama@gmail.com>
2023-01-15 23:28:34 +0100

    more fixes with more fun

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export const getLatLng = async (text) => {
 		if (text.slice(0, 20) === "https://www.google.c") {
 			const regex = /-?(\d*\.)?\d+,\s*-?(\d*\.)?\d+/
 			let result = text.match(regex);
-	
+			
 			if (result.length >= 0) {
 				return {
 					lng: Number.parseFloat(result[0].split(',')[0]),
@@ -20,7 +20,7 @@ export const getLatLng = async (text) => {
 			if (url) {
 				const regex = /-?(\d*\.)?\d+,\s*-?(\d*\.)?\d+/
 				let result = url.match(regex);
-			  	if (result.length >= 0) {
+				if (result.length >= 0) {
 					return {
 						lng: Number.parseFloat(result[0].split(',')[0]),
 						lat: Number.parseFloat(result[0].split(',')[1]),
@@ -30,8 +30,14 @@ export const getLatLng = async (text) => {
 			}
 			else throw new Error("This url can't be expanded")	
 		}
-	
+		
 	} catch (err) {
-	  return err
+		return err
 	}
 }
+
+const text = "https://goo.gl/maps/PUw9BjzaG7USfDiw7"
+
+getLatLng(text).then(data => {
+	console.log(data)
+})
